5419a3881ebdfba09f906d936563f3033afbaa3f
Natalia Lozano <acuarionat2004@gmail.com>
2024-04-23 20:01:58 -0400

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/social-links-profile-main-react/',
-  plugins: [react()],
-})
